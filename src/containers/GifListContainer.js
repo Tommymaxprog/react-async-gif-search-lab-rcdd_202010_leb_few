5a719e7ca8cd70525faec415bf342ba6c799583a
sagacious-shell-7968 <tommysytc.tc@gmail.com>
2021-01-15 06:41:47 +0000

    Automatically backed up by Learn

--- src/containers/GifListContainer.js
+++ src/containers/GifListContainer.js
@@ -11,13 +11,13 @@ class GifListContainer extends Component {
   render() {
     return(
       <div>
-        <GifSearch fetchGIFs={this.fetchGIFs} />
+        <GifSearch fetchGifs={this.fetchGifs} />
         <GifList gifs={this.state.gifs} />
       </div>
     )
   }
 
-  fetchGIFs = (query) => {
+  fetchGifs = (query) => {
     fetch(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC&rating=g`)
       .then(res => res.json())
       .then(({data}) => {
